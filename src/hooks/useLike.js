6da3feeb26fc9ec917ccdefe2e2fdee61abf4ac1
Dima163163 <khlopovdima@gmail.com>
2024-02-18 16:09:26 +0400

    course fix like

--- src/hooks/useLike.js
+++ src/hooks/useLike.js
@@ -2,18 +2,12 @@ import {useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 
 export const useLike = (statusLike) => {
-  console.log('statusLike: ', statusLike);
-  const token = useSelector(state => state.token.token);
   const {data: dataLike} = useSelector(state => state.like);
-  const [isLiked, setIsLiked] = useState(statusLike || false);
+  const [isLiked, setIsLiked] = useState(statusLike);
 
   useEffect(() => {
-    if (!token) {
-      setIsLiked(false);
-    } else {
-      setIsLiked(statusLike);
-    }
-  }, [setIsLiked, token]);
+    setIsLiked(statusLike);
+  }, [setIsLiked]);
 
   const changeLiked = () => setIsLiked(!isLiked);
 
